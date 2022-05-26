967bec6a7484da6899c2f462c508757017d55831
Alexandre Vieira <kitodann@gmail.com>
2022-05-26 16:08:15 -0300

    created the lists controller and the index view + listing all the movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+require 'json'
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+uri = URI.open(url).read
+response = JSON.parse(uri)
+
+create_movies = response['results']
+
+create_movies.each do |m|
+  movie = Movie.create(
+    title: m['title'],
+    overview: m['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{m['poster_path']}",
+    rating: m['vote_average']
+  )
+  puts movie.title
+end
+
+# puts response['results'][0]['title']
